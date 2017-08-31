00b2c7256e4b0dd2d28a0b8695a22e4e32593885
Gabriel Browarnik <gabrielbrowar@gmail.com>
2017-08-31 16:28:48 -0300

    adding authentication module and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_validation :generate_verification_code, on: :create
+
+  def generate_verification_code
+    self.verification_code = AuthenticableEntity.verification_code
+  end
 end
