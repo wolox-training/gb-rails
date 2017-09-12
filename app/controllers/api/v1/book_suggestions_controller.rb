e380f4c16c95191cd2a349581e12ec59b2f9a2cc
Gabriel Browarnik <gabrielbrowar@gmail.com>
2017-09-12 13:15:34 -0300

    avoiding sending user_id inside body on request

--- app/controllers/api/v1/book_suggestions_controller.rb
+++ app/controllers/api/v1/book_suggestions_controller.rb
@@ -1,21 +1,21 @@
 module Api
   module V1
     class BookSuggestionsController < ApiController
-      skip_before_action :authenticate_request
+      skip_before_action :authenticate_request, only: :create
 
       def create
-        @book_suggestion = BookSuggestion.new(book_suggestion_params)
-        @book_suggestion.user_id = current_user.try(:id) || nil
-        if @book_suggestion.save
-          render json: @book_suggestion, status: :created
+        book_suggestion = BookSuggestion.new(book_suggestion_params
+          .merge(user_id: current_user.try(:id)))
+        if book_suggestion.save
+          render json: book_suggestion, status: :created
         else
-          render json: { errors: @book_suggestion.errors }, status: :bad_request
+          render json: { errors: book_suggestion.errors }, status: :bad_request
         end
       end
 
       def book_suggestion_params
         params.require(:book_suggestion)
-              .permit(:user_id, :author, :link, :year, :publisher, :editorial, :price, :title)
+              .permit(:author, :link, :year, :publisher, :editorial, :price, :title)
       end
     end
   end
